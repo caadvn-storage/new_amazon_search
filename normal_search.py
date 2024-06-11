041b6d9c4c76d25631a2e042b1599ad01fc54189
long-npl <nguyen_phi_long@ca-adv.co.jp>
2024-06-11 10:36:41 +0900

    upload code

---
+++ normal_search.py
@@ -0,0 +1,127 @@
+import multiprocessing as mp
+import numpy as np
+import pandas as pd
+from commons.get_driver import *
+import logging
+
+
+# LOCAL_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
+LOCAL_PATH = os.getcwd()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler("log.txt", mode='w')
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s', datefmt="%H:%M")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+# LOCAL_PATH = os.getcwd()
+today_string = datetime.now().strftime("%Y%m%d")
+time_string = datetime.now().strftime("%H")
+today_folder = os.path.join(LOCAL_PATH, today_string)
+time_folder = os.path.join(today_folder, time_string + "時")
+os.makedirs(today_folder, exist_ok=True)
+os.makedirs(time_folder, exist_ok=True)
+
+
+def read_script(filename):
+    with open(os.path.join(LOCAL_PATH, '00_setting', filename), "r", encoding="UTF-8", errors="ignore") as file:
+        script = file.read()
+    return script
+
+
+def read_crawl_list():
+    df = pd.read_excel(os.path.join(
+        LOCAL_PATH, "crawling_list.xlsx"), engine='openpyxl', sheet_name="KEYWORD")
+    sub = df[df['KEYWORD'] != ""]
+    list_keys = list(sub['KEYWORD'])
+    return list_keys
+
+
+def get_info(script):
+    return read_script(script)
+
+
+def load_webpage(browsers, script):
+    return json.loads(browsers.driver.execute_script(script))
+
+
+def dataframe(json_change):
+    return pd.DataFrame(json_change[1:], columns=json_change[0])
+
+
+def get_data(lit_keys: list):
+    process_name = mp.current_process().name
+    browsers = WebDriver(exe_path=os.path.join(LOCAL_PATH, "00_setting", "chrome_driver.exe"))
+    # read scripts
+    crawl_script_1 = get_info('node_sb_top.txt')
+    crawl_script_2 = get_info('node_sp_o_mid.txt')
+    crawl_script_3 = get_info('node_sp_mid.txt')
+    crawl_script_4 = get_info('node_sb_mid_bottom.txt')
+    index = 0
+    for keys in lit_keys:
+        index = index + 1 
+        print(f'{process_name} : {index}/{len(lit_keys)} : {keys}')
+        browsers.driver.get(f"https://www.amazon.co.jp/s?k={keys}")
+        sleep(1)
+        browsers.wait_redirected()
+        # load webpages
+        response_1 = load_webpage(browsers, crawl_script_1)
+        sleep(0.5)
+        response_2 = load_webpage(browsers, crawl_script_2)
+        sleep(0.5)
+        response_3 = load_webpage(browsers, crawl_script_3)
+        sleep(0.5)
+        response_4 = load_webpage(browsers, crawl_script_4)
+        sleep(0.5)
+        # create dataframes
+        df_1 = dataframe(response_1)
+        df_2 = dataframe(response_2)
+        df_3 = dataframe(response_3)
+        df_4 = dataframe(response_4)
+        lst_df = []
+        if len(df_1) > 0:
+            lst_df.append(df_1[:3])
+        if len(df_2) > 0:
+            lst_df.append(df_2[:10])
+        if len(df_3) > 0:
+            lst_df.append(df_3[:5])
+        if len(df_4) > 0:
+            lst_df.append(df_4[:1])
+        if len(df_2) > 0:
+            lst_df.append(df_2[10:])
+        if len(df_4) > 1:
+            lst_df.append(df_4[1:])
+        if len(df_3) > 5:
+            lst_df.append(df_3[5:])
+
+        df_final = pd.concat(lst_df, ignore_index=True)
+        df_final.to_csv(os.path.join(time_folder, f"{today_string}_{time_string}時_{keys}.csv"),
+                        encoding='utf-16', sep="\t", index=False)
+    browsers.quit()
+
+
+if __name__ == '__main__':
+    mp.freeze_support()
+    try:
+        this_start = datetime.now()
+        print("==========START==========")
+        lst_keys = read_crawl_list()
+        WORKERS = 2
+        if len(lst_keys) < 10:
+            get_data(lst_keys)
+        else:
+            list_of_lists = np.array_split(lst_keys, WORKERS)
+            # converting numpy arrays to lists
+            output = [lst.tolist() for lst in list_of_lists]
+            with mp.Pool(WORKERS) as pool:
+                pool.map(get_data, output)
+
+        this_end = datetime.now()
+
+        print(f"COMPLETED, {this_start.strftime('%H:%M:%S')}~{this_end.strftime('%H:%M:%S')}, "
+                    f"in {timedelta(seconds=int((this_end - this_start).total_seconds()))} seconds")
+    except Exception as total_e:
+        print("TOTAL ERROR", total_e)
---
+++ ranking_sreach.py
@@ -0,0 +1,128 @@
+
+from commons.get_driver import *
+import numpy as np
+import multiprocessing as mp
+import sys
+import pandas as pd
+import logging
+
+
+# LOCAL_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
+LOCAL_PATH = os.getcwd()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler("log.txt", mode='w')
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s', datefmt="%H:%M")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+today_string = datetime.now().strftime("%Y%m%d")
+time_string = datetime.now().strftime("%H")
+today_folder = os.path.join(LOCAL_PATH, today_string)
+time_folder = os.path.join(today_folder, time_string + "時_Rank")
+os.makedirs(today_folder, exist_ok=True)
+os.makedirs(time_folder, exist_ok=True)
+
+
+def read_script(filename):
+    with open(os.path.join(LOCAL_PATH, '00_setting', filename), "r", encoding="UTF-8", errors="ignore") as file:
+        script = file.read()
+    return script
+
+
+def read_crawl_list():
+    df = pd.read_excel(os.path.join(
+        LOCAL_PATH, "crawling_list.xlsx"), engine='openpyxl', sheet_name="RANKING")
+    sub = df[df['RANKING_URL'] != ""]
+    list_links = list(sub['RANKING_URL'])
+    return list_links
+
+
+def get_info(script):
+    return read_script(script)
+
+
+def load_webpage(browsers, script):
+    return json.loads(browsers.driver.execute_script(script))
+
+
+def dataframe(json_change):
+    return pd.DataFrame(json_change[1:], columns=json_change[0])
+
+
+def get_data(list_links: list):
+    global category_name
+    process_name = mp.current_process().name
+    browsers = WebDriver()
+    # read scripts
+    crawl_script_1 = get_info('crawl_rank.txt')
+
+    page_lst = ["?ie=UTF8&ref_=sv_hpc_1", "ref=zg_bs_pg_2_hpc?ie=UTF8&pg=2"]
+    index = 0
+    for links in list_links:
+        index = index + 1 
+        print(f'{process_name} : {index}/{len(list_links)} : {links}')
+        lst_df = []
+        for page in page_lst:
+            if links.endswith("/"):
+                url = links + page
+            else:
+                url = links + "/" + page
+            browsers.driver.get(url)
+            category_name = browsers.get_element_by_xpath(
+                '//h1[@class="a-size-large a-spacing-medium a-text-bold"]').text.split("の")[0]
+            browsers.wait_redirected()
+            browsers.driver.execute_script('window.scrollBy(0, 500000);')
+            sleep(1)
+            browsers.wait_redirected()
+            try:
+                browsers.wait_element_by_xpath('.//div[@id="gridItemRoot"][50]')
+            except:
+                print("50下")
+            # load webpages
+            response_1 = load_webpage(browsers, crawl_script_1)
+
+            # create dataframes
+            df_1 = dataframe(response_1)
+
+            if len(df_1) > 0:
+                lst_df.append(df_1)
+                if len(df_1) < 50:
+                    break
+            
+        df_final = pd.concat(lst_df, ignore_index=True)
+        category_id = links.split("/")[-2]
+        file_name = f"{today_string}_{time_string}時_{category_name}({category_id}).csv"
+        df_final.to_csv(os.path.join(time_folder, file_name),
+                        encoding='utf-16', sep="\t", index=False)
+    browsers.quit()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename="log.txt", filemode='w',
+                    level="INFO", format='%(asctime)s - %(name)s - %(message)s', datefmt="%H:%M")
+    mp.freeze_support()
+    try:
+        this_start = datetime.now()
+        print("==========START==========")
+        list_links = read_crawl_list()
+        WORKERS = 2
+        if len(list_links) < 10:
+            get_data(list_links)
+        else:
+            list_of_lists = np.array_split(list_links, WORKERS)
+            # converting numpy arrays to lists
+            output = [l.tolist() for l in list_of_lists]
+            with mp.Pool(WORKERS) as pool:
+                pool.map(get_data, output)
+
+        this_end = datetime.now()
+
+        print(f"COMPLETED, {this_start.strftime('%H:%M:%S')}~{this_end.strftime('%H:%M:%S')}, "
+                    f"in {timedelta(seconds=int((this_end - this_start).total_seconds()))} seconds")
+    except Exception as total_e:
+        print("TOTAL ERROR", total_e)
+
